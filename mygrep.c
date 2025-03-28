63703d8598898b9219cbd3efe80ea2a68dd2f9cd
anamrtn <100523111@alumnos.uc3m.es>
2025-03-28 19:12:42 +0100

    Update mygrep.c

--- mygrep.c
+++ mygrep.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUFFER_SIZE 1024
 
 int main(int argc, char ** argv) {
     if (argc != 3) {
@@ -11,36 +12,38 @@ int main(int argc, char ** argv) {
     
     
     //Open file using fopen()
-    FILE* file = fopen(argv[1], "r");
-    if (!file){
+    int file = open(argv[1], O_RDONLY);
+    if (file < 0){
       perror("Error opening the file");
       return -1;
     }
     
-    char line[1024];
+    char line[BUFFER_SIZE+1];
     int found = 0;
     
     //Read wach line with fgets()
-    while (fgets(line, sizeof(line), file)){
+    while ((bytes_read = read(file, line, BUFFER_SIZE)) > 0){
+        line[bytes_read] = '\0'
       //Check for the substring using strstr()
       if (strstr(line, argv[2])){
         printf("%s", line);  //If found, print the line, if not, print <string> not found.
         found = 1;
       }
     }
+
+    if(bytes_read == -1){
+        perror("Error reading the file");
+    }
     
-
-
     if (!found){
       printf("%s not found. \n", argv[2]);
     }
+
     
-    
-    
-    if (fclose(file)==EOF){
+    if (close(file)<0){
       perror("Error closing the file");
       return -1;
-    };  
+    }
     
     
     return 0;
